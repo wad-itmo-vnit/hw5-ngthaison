9d3a919711db9349543e56a2345dc39a448761de
thaisonitmo <nguyenthaison015297@gmail.com>
2021-08-31 15:34:27 +0300

    Completely

--- main.py
+++ main.py
@@ -2,27 +2,25 @@ from flask import Flask, render_template, request, redirect, make_response, flas
 from functools import wraps
 from model.user import User
 import app_config
-import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = app_config.SECRET_KEY
 
-# users = {}
-# users['admin'] = User('admin', 'admin')
-
-users = {name[:-5]:User.from_file(name) for name in os.listdir(app_config.USER_DB_DIR)}
-
-def generate_token(user, pwd):
-    return user + ":" + pwd
+def check_cookie(req):
+    return User.get_user(req.cookies.get('username')).authorize(req.cookies.get('token')) 
 
 def login_required(func):
     @wraps(func)
     def login_func(*arg, **kwargs):
         try:
-            if (users[request.cookies.get('username')].authorize(request.cookies.get('token'))):
+            if check_cookie(request):
+                print('asd')
                 return func(*arg, **kwargs)
         except:
             pass
+        print('asdd')
+        print(request.cookies.get('username'))
+        print(request.cookies.get('token'))
         flash("Login required!")
         return redirect('/login')
     return login_func
@@ -31,7 +29,7 @@ def no_login(func):
     @wraps(func)
     def no_login_func(*arg, **kwargs):
         try:
-            if (users[request.cookies.get('username')].authorize(request.cookies.get('token'))):
+            if check_cookie(request):
                 flash("You've already logged in!")
                 return redirect('/index')
         except:
@@ -49,12 +47,13 @@ def home():
 def login():
     if request.method =='GET':
         return render_template('login.html')
+
     user = request.form.get('username')
     pwd = request.form.get('password')
-    if user in users.keys():
-        current_user = users[user]
-        if users[user].authenticate(pwd):
-            token = users[user].init_session()
+    if User.find_user(user): 
+        current_user = User.get_user(user)
+        if current_user.authenticate(pwd):
+            token = current_user.init_session()
             resp = make_response(redirect('/index'))
             resp.set_cookie('username', user)
             resp.set_cookie('token', token)
@@ -75,7 +74,8 @@ def index():
 @login_required
 def logout():
     username = request.cookies.get('username')
-    users[username].terminate_session()
+    current_user = User.get_user(username)
+    current_user.terminate_session()
     resp = make_response(redirect('/login'))
     resp.delete_cookie('username')
     resp.delete_cookie('token')
@@ -90,10 +90,10 @@ def register():
 
     username, password, password_confirm = request.form.get('username'), request.form.get('password'), request.form.get('password_confirm')
 
-    if username not in users.keys():
+    if not User.find_user(username):
         if password == password_confirm:
-            users[username] = User.new(username, password)
-            token = users[username].init_session()
+            new_user = User.new(username, password)
+            token = new_user.init_session()
             resp = make_response(redirect('/index'))
             resp.set_cookie('username', username)
             resp.set_cookie('token', token) 
@@ -104,6 +104,29 @@ def register():
         flash("User already exists!")
 
     return render_template('register.html')
-     
+
+@app.route('/changepwd', methods=['POST', 'GET'])
+@login_required
+def changepwd():
+    if request.method == "GET":
+        return render_template("changepwd.html")
+
+    username = request.cookies.get("username")
+    old_pwd = request.form.get('old_pwd')
+    new_pwd = request.form.get('new_pwd')
+    new_pwd_confirm = request.form.get('new_pwd_confirm')
+
+    current_user = User.get_user(username)
+    if current_user.authenticate(old_pwd):
+        if new_pwd == new_pwd_confirm:
+            current_user.update_password(new_pwd)
+            flash("Password updated successfully!")
+            return redirect('/')
+        else:
+            flash("New passwords don't match")
+    else:
+        flash("Old password is not correct!")
+    return render_template("changepwd.html")
+
 if __name__ == '__main__':
     app.run(host='localhost', port='5000', debug=True)
--- model/user.py
+++ model/user.py
@@ -1,6 +1,8 @@
+import os
 import random
 import string
 import app_config
+import os
 from werkzeug.security import generate_password_hash, check_password_hash
 
 def gen_session_token(length=24):
@@ -19,7 +21,7 @@ class User:
         password = generate_password_hash(password)
         return cls(username, password)
 
-    classmethod
+    @classmethod
     def from_file(cls, filename):
         with open(app_config.USER_DB_DIR + '/' + filename, 'r') as f:
             text = f.readline().strip()
@@ -28,13 +30,26 @@ class User:
                 return cls(username, password)
             return cls(username, password, token)
 
+    @staticmethod
+    def find_user(username):
+        usernames = set(name[:-5] for name in os.listdir(app_config.USER_DB_DIR))
+        return username in usernames
+
+    @classmethod
+    def get_user(cls, username):
+        return cls.from_file(username + ".data")         
+
     def authenticate(self, password):
         return check_password_hash(self.password, password)
 
+    def update_password(self, password):
+        self.password = generate_password_hash(password)
+        self.dump()
+
     def init_session(self):
         self.token = gen_session_token()
-        return self.token
         self.dump()
+        return self.token
 
     def authorize(self, token):
         return token == self.token
